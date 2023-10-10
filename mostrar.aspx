a5a2a4ad8e5ae07ce4b291d5448b3e1655cd413c
Fquinde <95882367+Fquinde@users.noreply.github.com>
2023-10-10 19:56:52 +0200

    Add files via upload

---
+++ mostrar.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="mostrar.aspx.cs" Inherits="mostrar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Facturas</title>
+</head>
+<style>
+    #mostrarfacturas {
+        justify-content: center;
+        display: flex;
+    }
+</style>
+ 
+
+<body>
+    <form id="form1" runat="server">
+        <div  style="text-align:center"; >
+            <h1 >Lista de Facturas</h1>
+            <asp:GridView  H="table table-secondary table-bordered border-3 table-striped" ID="mostrarfacturas"  runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" >
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+            <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/datos/XMLFile.xml"></asp:XmlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ mostrar.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+public partial class mostrar : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable datos = new DataTable("Facturas");
+        DataColumn datoscolumnas;
+        DataRow dtRow;
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "numero";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "fecha";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "cif_cliente";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "nombre_cliente";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "importe";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "importe_iva";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "moneda";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "fecha_cobro";
+        datos.Columns.Add(datoscolumnas);
+
+        datoscolumnas = new DataColumn();
+        datoscolumnas.DataType = typeof(String);
+        datoscolumnas.ColumnName = "estado";
+        datos.Columns.Add(datoscolumnas);
+
+
+        DataSet tabla = new DataSet();
+        tabla.Tables.Add(datos);
+
+
+        DataSet datosXml = new DataSet();
+        datosXml.ReadXml(Server.MapPath("~/datos/Facturas.xml"));
+        
+
+        // Recorrer los datos
+        for (int x = 0; x <= datosXml.Tables.Count - 1; x++)
+        {
+            // recorre las filas 
+            for (int y = 0; y <= datosXml.Tables[x].Rows.Count - 1; y++)
+            {
+                
+                    dtRow = datos.NewRow();
+                    dtRow["numero"] = datosXml.Tables[x].Rows[y].ItemArray[0];
+                    dtRow["fecha"] = datosXml.Tables[x].Rows[y].ItemArray[1];
+                    dtRow["cif_cliente"] = datosXml.Tables[x].Rows[y].ItemArray[2];
+                    dtRow["nombre_cliente"] = datosXml.Tables[x].Rows[y].ItemArray[3];
+                    dtRow["importe"] = datosXml.Tables[x].Rows[y].ItemArray[4];
+                    dtRow["importe_iva"] = datosXml.Tables[x].Rows[y].ItemArray[5];
+                    dtRow["moneda"] = datosXml.Tables[x].Rows[y].ItemArray[6];
+                    dtRow["fecha_cobro"] = datosXml.Tables[x].Rows[y].ItemArray[7];
+                    dtRow["estado"] = datosXml.Tables[x].Rows[y].ItemArray[8];
+
+                    datos.Rows.Add(dtRow);
+            }
+        }   
+
+        mostrarfacturas.DataSource = tabla;
+        mostrarfacturas.DataBind();
+
+
+    }
+
+
+}
